d96b5ee2e75114880efb6bee0320cf65edc61a3d
Alexandr Terehov <donpearlon@gmail.com>
2023-03-01 19:45:19 +0300

    ConditionalOnMissingBean added

--- 1-Spring-Foundation/h2-datasource-starter/src/main/java/com/aterehov/DataSourceConfig.java
+++ 1-Spring-Foundation/h2-datasource-starter/src/main/java/com/aterehov/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package com.aterehov;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -15,6 +16,7 @@ public class DataSourceConfig {
     private Environment environment;
 
     @Bean
+    @ConditionalOnMissingBean
     public DataSource dataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(H2_DRIVER_CLASS_NAME);
