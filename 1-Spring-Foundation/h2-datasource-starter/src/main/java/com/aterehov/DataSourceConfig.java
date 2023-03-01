07575b8a3fcddeff37d0ee1debee962bf5dd0b15
Alexandr Terehov <donpearlon@gmail.com>
2023-03-01 19:34:22 +0300

    Datasource config refactored

--- 1-Spring-Foundation/h2-datasource-starter/src/main/java/com/aterehov/DataSourceConfig.java
+++ 1-Spring-Foundation/h2-datasource-starter/src/main/java/com/aterehov/DataSourceConfig.java
@@ -3,7 +3,6 @@ package com.aterehov;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -11,32 +10,19 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+    public static final String H2_DRIVER_CLASS_NAME = "org.h2.Driver";
     @Autowired
     private Environment environment;
 
     @Bean
-    @Primary
     public DataSource dataSource() {
-        System.out.println("!!!!!!!!!CREATING DATASOURCE!!!!!!!!!!!");
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:demo_db;DB_CLOSE_DELAY=-1");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
+        dataSource.setDriverClassName(H2_DRIVER_CLASS_NAME);
+        dataSource.setUrl(environment.getProperty("h2-datasource.url"));
+        dataSource.setUsername(environment.getProperty("h2-datasource.username"));
+        dataSource.setPassword(environment.getProperty("h2-datasource.password"));
 
         return dataSource;
     }
 
-    /*
-        @Bean
-    public DataSource dataSource() {
-        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-        dataSource.setUrl(env.getProperty("jdbc.url"));
-        dataSource.setUsername(env.getProperty("jdbc.user"));
-        dataSource.setPassword(env.getProperty("jdbc.pass"));
-
-        return dataSource;
-    }
-     */
 }
